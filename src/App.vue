59622ae0de03c3734d19447c0be9ef212698bb9b
MaickDavila <librajunior13@gmail.com>
2022-04-21 19:58:15 -0500

    con login

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
+<script lang="ts" setup>
+import { useUserStore } from './store/user'
+const router = useRoute()
+const userStore = useUserStore()
+
+onMounted(() => {
+  setTimeout(() => {
+    const path = router.fullPath
+    if (path === '/')
+      localStorage.removeItem('userpgtoken')
+  }, 500)
+
+  const token = localStorage.getItem('userpgtoken') as string
+
+  if (token)
+    userStore.get_user_data_by_token()
+})
+</script>
 <template>
   <router-view />
 </template>
